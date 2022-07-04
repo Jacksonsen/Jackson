e7b3030fac6e12baf0a71a7f2ade1d0d05693aba
Jacksonsen <cs174383@163.com>
2022-07-04 18:06:29 +0800

    Update 清理lsof.sh

--- 清理lsof.sh
+++ 清理lsof.sh
@@ -4,7 +4,8 @@
 process_id=`sudo lsof +L1 |grep *需要过滤的文件名* |grep -v "grep" | awk '{print $2}'`
 echo $process_id
 
-echo "---------------"
+echo "-----------------------------"
+echo "-----------------------------"
 
 
 for id in $process_id
